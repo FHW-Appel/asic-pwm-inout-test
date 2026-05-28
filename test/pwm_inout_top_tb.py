ab24442564e312766009b65ddd0f952ae116f2b0
FHW-Appel <appel@fh-westkueste.de>
2026-05-28 16:11:50 +0200

    pwn_gen successfully tested

--- test/pwm_inout_top_tb.py
+++ test/pwm_inout_top_tb.py
@@ -7,6 +7,16 @@ async def test_pwm_inout_top(dut):
     await initialization(dut)
     cocotb.start_soon(Clock(dut.pwm_in, 3, unit="ms", period_high=1.5).start())
     await Timer(5, unit="ms")
+    dut._log.info("5 ms passed")
+    await Timer(10, unit="ms")
+    dut._log.info("15 ms passed")
+    await Timer(10, unit="ms")
+    dut._log.info("25 ms passed")
+    await Timer(10, unit="ms")
+    dut._log.info("35 ms passed")
+    await Timer(10, unit="ms")
+    dut._log.info("45 ms passed")
+
     assert dut.opins.value == 50, f"Expected opins to be 50, got {dut.opins.value}"
 
 async def initialization(dut):
@@ -15,9 +25,9 @@ async def initialization(dut):
     # Initialize SPI signals to avoid X values
     dut.spi_cs_n.value = 1
     dut.spi_mosi.value = 0
-    dut.ipins.value = 20
+    dut.ipins.value = 99
     dut.rst_n.value = 0
-    await Timer(20, unit="ns")
+    await Timer(200, unit="ns")
     dut.rst_n.value = 1
     await RisingEdge(dut.clk)
     
